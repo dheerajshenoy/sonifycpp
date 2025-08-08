2e918cc86a859bbe39c3c0029549f5d76ea7c047
Dheeraj Vittal Shenoy <dheerajshenoy22@gmail.com>
2025-08-08 13:47:39 +0530

    Add intensity function

--- src/utils.hpp
+++ src/utils.hpp
@@ -5,7 +5,8 @@
 #include <QStringView>
 #include <vector>
 
-namespace utils {
+namespace utils
+{
 
     // ------- Image processing -------
     QImage changeGamma(const QImage &image, int gamma) noexcept;
@@ -16,35 +17,22 @@ namespace utils {
     QImage changeContrast(const QImage &image, int delta) noexcept;
 
     // ------- Math and mapping -------
-    short LinearMap(double value,
-                    double in_min,
-                    double in_max,
-                    double out_min,
+    short LinearMap(double value, double in_min, double in_max, double out_min,
                     double out_max) noexcept;
 
-    short ExpMap(double value,
-                 double in_min,
-                 double in_max,
-                 double out_min,
+    short ExpMap(double value, double in_min, double in_max, double out_min,
                  double out_max) noexcept;
 
-    short LogMap(double value,
-                 double in_min,
-                 double in_max,
-                 double out_min,
+    short LogMap(double value, double in_min, double in_max, double out_min,
                  double out_max) noexcept;
 
     // ------- Signal generation -------
-    void generateSineWave(std::vector<short> &buffer,
-                          double amplitude,
-                          double frequency,
-                          double time,
+    void generateSineWave(std::vector<short> &buffer, double amplitude,
+                          double frequency, double time,
                           float samplerate) noexcept;
 
-    std::vector<short> sineWave(double _amplitude,
-                                double frequency,
-                                double time,
-                                float samplerate) noexcept;
+    std::vector<short> sineWave(double _amplitude, double frequency,
+                                double time, float samplerate) noexcept;
 
     void applyEnvelope(std::vector<short> &samples) noexcept;
 
@@ -55,6 +43,8 @@ namespace utils {
     // ------- Utility -------
     double Hue2Freq(double hue) noexcept;
 
+    int intensity(QRgb pixel) noexcept;
+
     std::vector<double> linspace(double start, double end, int num) noexcept;
 
     std::vector<short> readWAVFile(const QString &path) noexcept;
@@ -62,7 +52,8 @@ namespace utils {
     // ------- Vector math (templates) -------
     template <typename T>
     std::vector<T> addTwoVectors(const std::vector<T> &first,
-                                 const std::vector<T> &second) noexcept {
+                                 const std::vector<T> &second) noexcept
+    {
         std::vector<T> result;
         result.reserve(first.size());
         for (size_t i = 0; i < first.size(); ++i)
@@ -72,21 +63,21 @@ namespace utils {
 
     // Base case: just return one vector
     template <typename T>
-    std::vector<T> addVectors(const std::vector<T> &v) noexcept {
+    std::vector<T> addVectors(const std::vector<T> &v) noexcept
+    {
         return v;
     }
 
     template <typename T, typename... Args>
     std::vector<T> addVectors(const std::vector<T> &first,
                               const std::vector<T> &second,
-                              const Args &...args) noexcept {
+                              const Args &...args) noexcept
+    {
         return addVectors(addTwoVectors(first, second), args...);
     }
 
     // Helper function to generate WAV file
-    bool generateWavFile(const QString &filename,
-                         int sampleRate,
-                         int duration,
+    bool generateWavFile(const QString &filename, int sampleRate, int duration,
                          const std::vector<short> &data) noexcept;
 
 } // namespace utils
